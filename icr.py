24b9cbea862fd90bdac74cad0f9ed1aac9ac0959
Victor Rodrigues <143040764+VictorRodrigues16@users.noreply.github.com>
2024-03-07 15:19:02 -0300

    Update icr.py

--- icr.py
+++ icr.py
@@ -1,7 +1,19 @@
 import tkinter as tk
-from tkinter import ttk
 from PIL import Image, ImageTk
 import ctypes
+
+class CustomButton(tk.Button):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.configure(
+            background='#6495ED',
+            foreground='white',
+            font=('Helvetica', 12, 'bold'),
+            relief=tk.GROOVE,
+            borderwidth=0,
+            padx=10,
+            pady=5
+        )
 
 def publicar():
     print("Botão Publicar pressionado")
@@ -14,8 +26,6 @@ def localizacao():
 
 def sair():
     root.destroy()
-
-
 
 root = tk.Tk()
 root.title("ICR CONECTA")
@@ -30,45 +40,31 @@ pos_x = (largura_tela - largura_janela) // 2
 pos_y = (altura_tela - altura_janela) // 2
 root.geometry(f"{largura_janela}x{altura_janela}+{pos_x}+{pos_y}")
 
-
-
-style = ttk.Style()
-style.configure('Custom.TButton', background='#6495ED', foreground='black', focuscolor='#6495ED',
-                font=('Helvetica', 12, 'bold'))
-
 image = Image.open("image 3.png")
-
 image = image.resize((300, 200))
 photo = ImageTk.PhotoImage(image)
-
-
 label_img = tk.Label(root, image=photo)
-
 label_img.place(relx=0.5, rely=0, anchor="n")
 
-
 espacamento = 20
-
-
 pos_y = 0.5
 
-
-btn_publicar = ttk.Button(root, text="Publicar", style='Custom.TButton', command=publicar)
+btn_publicar = CustomButton(root, text="Publicar", command=publicar)
 btn_publicar.place(relx=0.5, rely=pos_y, anchor="center", width=200, height=50)
 
 pos_y += 0.1 + espacamento / 500
 
-btn_procedimentos = ttk.Button(root, text="Procedimentos", style='Custom.TButton', command=procedimentos)
+btn_procedimentos = CustomButton(root, text="Procedimentos", command=procedimentos)
 btn_procedimentos.place(relx=0.5, rely=pos_y, anchor="center", width=200, height=50)
 
 pos_y += 0.1 + espacamento / 500
 
-btn_localizacao = ttk.Button(root, text="Localização", style='Custom.TButton', command=localizacao)
+btn_localizacao = CustomButton(root, text="Localização", command=localizacao)
 btn_localizacao.place(relx=0.5, rely=pos_y, anchor="center", width=200, height=50)
 
 pos_y += 0.1 + espacamento / 500
 
-btn_sair = ttk.Button(root, text="Sair", style='Custom.TButton', command=sair)
+btn_sair = CustomButton(root, text="Sair", command=sair)
 btn_sair.place(relx=0.5, rely=pos_y, anchor="center", width=200, height=50)
 
 root.mainloop()
